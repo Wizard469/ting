46f5dd330b54fde79442d208c3bf8d439ffb8069
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2023-04-06 10:16:12 -0300

    Req 2 - Implementing the function txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        with open(path_file, "r") as f:
+            if path_file.endswith(".txt"):
+                lines = f.read().splitlines()
+                return lines
+            else:
+                print("Formato inválido", file=sys.stderr)
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)

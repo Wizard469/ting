317cff82b258c766633666fd57f6f790ec722a42
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2023-04-06 18:04:08 -0300

    Req 3 - Implementing the function process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,25 @@
+import sys
+from .file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    data = txt_importer(path_file)
+    if data is None:
+        return
+
+    file_info = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data
+    }
+
+    for item in instance.items:
+        if item["nome_do_arquivo"] == path_file:
+            print(f"File '{path_file}' already processed", file=sys.stdout)
+            return
+
+    instance.enqueue(file_info)
+    print(file_info, file=sys.stdout)
 
 
 def remove(instance):

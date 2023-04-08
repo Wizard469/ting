93cbe8b5417b360b512ca625dca3f142d07c449b
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2023-04-08 14:47:55 -0300

    Req 7 - Implementing the function exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    search = []
+    word = word.lower()
+
+    for item in instance.items:
+        occurrences = []
+        for line_number, line in enumerate(item["linhas_do_arquivo"], 1):
+            if word in line.lower():
+                occurrences.append({
+                        "linha": line_number
+                    })
+        if occurrences:
+            search.append({
+                "palavra": word,
+                "arquivo": item["nome_do_arquivo"],
+                "ocorrencias": occurrences
+            })
+
+    return search
 
 
 def search_by_word(word, instance):
